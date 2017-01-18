8ac7a412e34d74fdcf7881f172bf7bcb6debaa4b
Zyrass <zyrass@outlook.fr>
2017-01-18 13:50:11 +0100

    correction de quelques erreur dans le code

--- projet-formulaire/saveIndex.php
+++ projet-formulaire/saveIndex.php
@@ -23,8 +23,8 @@
     </head>
     <body>
         <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
+<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
 
         <div class="container">
             <div class="divider"></div>
@@ -34,37 +34,37 @@
 
             <div class="row">
                 <div class="col-lg-10 col-lg-offset-1">
-                    <form action="" id="contact-form" method="POST" role="form">
+                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ; ?>" id="contact-form" method="POST" role="form">
                         <div class="row">
                             <!-- Prénom -->
                             <div class="col-md-6">
                                 <label for="firstname">Prénom <span class="blue">*</span></label>
-                                <input type="text" name="firstname" class="form-control" placeholder="Votre prénom" id="firstname"  value="">
-                                <p class="comments"></p>
+                                <input type="text" name="firstname" class="form-control" placeholder="Votre prénom" id="firstname" required value="<?php echo $firstname; ?>">
+                                <p class="comments"><?php cho $firstnameError; ?></p>
                             </div>
                             <!-- Nom -->
                             <div class="col-md-6">
                                 <label for="lastname">Nom <span class="blue">*</span></label>
-                                <input type="text" name="lastname" class="form-control" placeholder="Votre Nom" id="lastname"  value="">
-                                <p class="comments"></p>
+                                <input type="text" name="lastname" class="form-control" placeholder="Votre Nom" id="lastname" required value="<?php echo $lastname; ?>">
+                                <p class="comments"><?php echo $lastnameError; ?></p>
                             </div>
                             <!-- E-mail -->
                             <div class="col-md-6">
-                                <label for="email">E-mail <span class="blue">*</span></label>
-                                <input type="email" name="email" class="form-control" placeholder="Votre E-mail" id="email"  value="">
-                                <p class="comments"></p>
+                                <label for="mail">E-mail <span class="blue">*</span></label>
+                                <input type="email" name="mail" class="form-control" placeholder="Votre E-mail" id="mail" required value="<?php echo $mail; ?>">
+                                <p class="comments"><?php echo $mailError; ?></p>
                             </div>
                             <!-- Téléphone -->
                             <div class="col-md-6">
                                 <label for="phone">Téléphone</label>
-                                <input type="tel" name="phone" class="form-control" placeholder="Votre Téléphone" id="phone" value="">
-                                <p class="comments"></p>
+                                <input type="tel" name="phone" class="form-control" placeholder="Votre Téléphone" id="phone" required value="<?php echo $phone; ?>">
+                                <p class="comments"><?php echo $phoneError; ?></p>
                             </div>
                             <!-- Message -->                        
                             <div class="col-md-12">
                                 <label for="message">Message <span class="blue">*</span></label>
-                                <textarea name="message" class="form-control" id="message" rows="4"  placeholder="Votre message ici..."></textarea>
-                                <p class="comments"></p>
+                                <textarea name="message" class="form-control" id="message" rows="4" required placeholder="Votre message ici..."><?php echo $message; ?></textarea>
+                                <p class="comments"><?php echo $messageError; ?></p>
                             </div>
                             <!-- Information requise -->
                             <div class="col-md-12">
@@ -75,11 +75,12 @@
                                 <input type="submit" class="button1" value="Envoyer">
                             </div>
                         </div>
+                        <p class="thank-you" style="display:<?php if($isSuccess) echo 'block'; else echo 'none'; ?>">Votre message a bien été envoyé. Merci de m'avoir contacté !</p>
                     </form>
                 </div>
             </div>
         </div>
-        
+
         <!-- JQuery -->
         <script type="text/javascript" src="vendor/js/jquery-3.1.1.min.js"></script>
         <!-- Script bootstrap 3.3.7 -->
